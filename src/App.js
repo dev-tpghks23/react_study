c3b87c84567c758c90bd2214b5168718212f0c0c
dev-tpghks23 <tpghksgit23@gmail.com>
2024-09-24 17:45:42 +0900

    day02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import Color from './day01/state/Color';
+// import Name from './day02/ref/basic/Name';
+// import Check from './day02/ref/basic/Check';
+// import FoodContainer from './day02/ref/expert/FoodContainer';
+import Checkbox from './day02/ref/expert/Checkbox';
+// import FoodContainer from './day02/map/basic/FoodContainer';
+// import UserContainer from './day02/map/normal/UserContainer';
+// import Color from './day01/state/Color';
 // import Count from './day01/state/Count';
 // import Sound from './day01/state/Sound';
 // import Container from './day01/props/Container';
@@ -12,7 +18,13 @@ function App() {
       {/* <Count /> */}
       {/* <Sound /> */}
       {/* <Name /> */}
-      <Color />
+      {/* <Color /> */}
+      {/* <FoodContainer /> */}
+      {/* <UserContainer /> */}
+      {/* <Name /> */}
+      {/* <Check /> */}
+      {/* <FoodContainer /> */}
+      <Checkbox />
     </>
   );
 }
